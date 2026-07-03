ad859079f25bf3a8a37f39113528461faf5f73ac
piotrbros <pibro@visa.com>
2026-07-03 18:50:31 +0200

    Added remove functionality, cleaned code.

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,6 +1,9 @@
 package pl.coderslab;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,12 +13,19 @@ import java.util.Scanner;
 
 public class TaskManager {
     public static void main(String[] args) {
-        String[] options = {"add", "remove", "list", "exit"};
+        final String[] OPTIONS = {"add", "remove", "list", "exit"};
+        final Path PATH = Paths.get("tasks.csv");
         Scanner scanner = new Scanner(System.in);
-        String[][] tasks = openTasks();
+        String[][] tasks;
+        try {
+            tasks = openTasks(PATH);
+        } catch (IOException e) {
+            System.out.println("Cannot open " + PATH.getFileName());
+            return;
+        }
 
         while (true) {
-            viewOptions(options);
+            viewOptions(OPTIONS);
             String userInput = scanner.nextLine();
 
             switch (userInput) {
@@ -25,32 +35,121 @@ public class TaskManager {
                 case "list":
                     listTasks(tasks);
                     break;
-                // other options
+                case "add":
+                    tasks = addTask(tasks);
+                    break;
+                case "remove":
+                    tasks = removeTask(tasks);
+
                 default:
                     System.out.println("Please select a correct option.");
             }
         }
 
 
+    }
 
-
+    public static String[][] removeTask(String[][] tasks) {
+        System.out.println("remove");
+        int indexToRemove = getIndexToRemove(tasks.length);
+        // Removing task with an index provided by user
+        return ArrayUtils.remove(tasks, indexToRemove);
 
 
     }
 
+    public static int getIndexToRemove(int tasksLength) {
+        // Validate and get user input
+        Scanner scanner = new Scanner(System.in);
+        int maxIndex = tasksLength - 1;
+        int index;
+        String inputError = "Incorrect argument passed. Please give a number between 0 and " + (maxIndex);
+
+        System.out.println("Please select a number to remove.");
+        while (true) {
+            try {
+                index = Integer.parseInt(scanner.nextLine());
+                if (index >= 0 && index <= maxIndex) {
+                    return index;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println(inputError);
+                continue;
+            }
+            System.out.println(inputError);
+        }
+    }
+
+
+    // This method adds new task to table and returns a new table with updated task list
+    public static String[][] addTask(String[][] tasks) {
+        System.out.println("add");
+
+        // Get info about new task from the user.
+        String[] newTask = getNewTask();
+
+        // Preparing a copy array with + 1 length to accommodate user input
+        String[][] newTasks = Arrays.copyOf(tasks, tasks.length + 1);
+
+        // Saving userInput into the copied array.
+        newTasks[tasks.length] = newTask;
+
+        //Returning final newTasks
+        return newTasks;
+    }
+
+    // This method gets new task info from the user, ensures proper data format and returns String[].
+    public static String[] getNewTask() {
+        String[] userInputDescription = {
+                "Please add task description",
+                "Please add task due date",
+                "Is your task important: true/false"
+        };
+        String[] userInput = new String[userInputDescription.length];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < userInput.length; i++) {
+            // Prompting the user for data
+            System.out.println(userInputDescription[i]);
+
+            // Collecting data from user input
+            while (true) {
+                String input = scanner.nextLine();
+
+                if (input.contains(",")) {
+                    System.out.println("Please dont use \",\" in the input.");
+                    continue;
+                }
+
+                userInput[i] = input;
+                break;
+            }
+        }
+        return userInput;
+    }
+
     public static void listTasks(String[][] tasks) {
         System.out.println("list");
+
+        if (tasks.length == 0) {
+            System.out.println("Your task list is empty!");
+        }
+
         StringBuilder buffer = new StringBuilder();
 
+        // For loop that creates every row of the output
         for (int i = 0; i < tasks.length; i++) {
             buffer.append(i);
             buffer.append(" : ");
+            // Inner loop that iterates over all columns in the row
             for (String column : tasks[i]) {
                 buffer.append(column);
                 buffer.append(" ");
             }
+            // Finishing row creation
             buffer.append("\n");
         }
+        // Printing the whole buffer
         System.out.println(buffer);
     }
 
@@ -70,22 +169,16 @@ public class TaskManager {
         }
     }
 
-    public static String[][] openTasks() {
-        Path path = Paths.get("tasks.csv");
+    public static String[][] openTasks(Path path) throws IOException {
         String[][] data;
 
         // Opening the tasks.csv file
-        try {
-            List<String> rows = Files.readAllLines(path);
+        List<String> rows = Files.readAllLines(path);
 
-            data = new String[rows.size()][];
-            for (int i = 0; i < rows.size(); i++) {
-                data[i] = rows.get(i).split(",");
-            }
-            return data;
-        } catch (IOException e) {
-            System.out.println("Error while reading: " + path.getFileName());
-            return new String[0][0];
+        data = new String[rows.size()][];
+        for (int i = 0; i < rows.size(); i++) {
+            data[i] = rows.get(i).split(",");
         }
+        return data;
     }
 }

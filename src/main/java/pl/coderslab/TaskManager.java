28e6b37a04b5109f7e544801ebcf7fe2c6004e2c
piotrbros <pibro@visa.com>
2026-07-03 13:03:42 +0200

    Added file loading and main loop that runs the program.

---
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -0,0 +1,79 @@
+package pl.coderslab;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class TaskManager {
+    public static void main(String[] args) {
+        String[] options = {"add", "remove", "list", "exit"};
+        Scanner scanner = new Scanner(System.in);
+
+
+        String[][] tasks = openTasks();
+
+        while (true) {
+            viewOptions(options);
+            String userInput = scanner.nextLine();
+
+            switch (userInput) {
+                case "exit":
+                    exitProgram();
+                    return;
+                case "print":
+                    for (String[] row : tasks) {
+                        System.out.println(Arrays.toString(row));
+                    }
+                    break;
+                // other options
+                default:
+                    System.out.println("Please select a correct option.");
+            }
+        }
+
+
+
+
+
+
+    }
+
+    public static void exitProgram() {
+        //Save file method below TO ADD
+        System.out.println("exit");
+        System.out.println(ConsoleColors.RED + "Bye, bye.");
+        return;
+    }
+
+    public static void viewOptions(String[] optionsList) {
+        System.out.println();
+        System.out.println(ConsoleColors.BLUE + "Please select an option:");
+
+        for (String option : optionsList) {
+            System.out.println(ConsoleColors.RESET + option);
+        }
+    }
+
+    public static String[][] openTasks() {
+        Path path = Paths.get("tasks.csv");
+        String[][] data;
+
+        // Opening the tasks.csv file
+        try {
+            List<String> rows = Files.readAllLines(path);
+
+            data = new String[rows.size()][];
+            for (int i = 0; i < rows.size(); i++) {
+                data[i] = rows.get(i).split(",");
+            }
+            return data;
+        } catch (IOException e) {
+            System.out.println("Error while reading: " + path.getFileName());
+            return new String[0][0];
+        }
+    }
+}
